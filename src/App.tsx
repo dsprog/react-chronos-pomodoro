7793a9570c0678b134174897058a234409eb3512
Daniel Santos <daniel.oliveira@abril.com.br>
2026-05-07 09:47:07 -0300

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
     return (
-        <div>
+        <>
             <h1> Olá mundo</h1>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique corporis porro eius, nam error a maiores laboriosam id sequi cumque culpa distinctio dolor placeat quas aperiam molestias iste sed voluptates!</p>
-        </div >
+        </>
     );
 }
 
