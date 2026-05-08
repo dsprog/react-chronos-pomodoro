cddfedf653d97b9c19a98ff615a596e86041053a
Dsprog <dsprog@gmail.com>
2026-05-07 23:27:00 -0300

    feat: implement main application layout with Logo, Menu, and CountDown components using lucide-react icons

--- src/main.tsx
+++ src/main.tsx
@@ -2,21 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { Container } from './components/Container'
+import { Logo } from './components/Logo'
+import { Menu } from './components/Menu'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 
 import './assets/css/themes.css'
 import './assets/css/global.css'
+import { CountDown } from './components/CountDown'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <>
       <Container>
-        <Header />
-        <App />
-        <Footer />
+        <Logo />
       </Container>
-      <Container>dsdas</Container>
+      <Container>
+        <Menu />
+      </Container>
+      <Container>
+        <CountDown />
+      </Container>
     </>
   </StrictMode>,
 )
